d29859c9c516026c483bc9499c5f6475f946ca16
vyrral <76436041+vyrral@users.noreply.github.com>
2025-06-18 15:00:40 +0000

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,7 @@ import SalaryPerHour from "./pages/SalaryPerHour";
 import Disclaimer from "./pages/Disclaimer";
 import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
-import WhatsAppSticky from "@/components/WhatsAppSticky";
-// ...in your layout or root component JSX:
-<WhatsAppSticky />
+
 const queryClient = new QueryClient();
 
 const App = () => (
